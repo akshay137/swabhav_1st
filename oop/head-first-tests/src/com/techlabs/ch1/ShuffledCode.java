3786674b0b03f3f676d0003963fbf006d40263fa
akshay137 <neutralasneutral@gmail.com>
2019-11-23 10:12:32 +0530

    chapter 1

---
+++ oop/head-first-tests/src/com/techlabs/ch1/ShuffledCode.java
@@ -0,0 +1,26 @@
+package com.techlabs.ch1;
+
+public class ShuffledCode {
+	public static void main(String[] args)
+	{
+		int x = 3;
+		while (x > 0)
+		{
+			if (x > 2)
+			{
+				System.out.print("a");
+			}
+			if (x == 2)
+			{
+				System.out.print("b c");
+			}
+			x = x - 1;
+			System.out.print("-");
+			if (x == 1)
+			{
+				System.out.print("d");
+				x = x - 1;
+			}
+		}
+	}
+}
