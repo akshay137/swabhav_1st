755a98a6cfb659ccd766256a56c0b5283ff5f457
akshay137 <neutralasneutral@gmail.com>
2019-12-19 15:19:13 +0530

    _

--- angularjs-app/student.js
+++ angularjs-app/student.js
@@ -12,31 +12,31 @@
 						.then(res => {
 							resolve({ msg: 'OK', data: res.data });
 						}).catch(err => {
-							reject({ msg: 'error', data: [] });
+							reject({ msg: 'error', data: [], err: err });
 						});
 				});
 			}
 
-			svc.addStudent = function (student) {
+			function sendData(data, method, link) {
 				return $q(function (resolve, reject) {
-					$http.post(api)
-						.then(res => {
-							resolve({ msg: 'OK', data: res.data });
-						}).catch(err => {
-							reject({ msg: 'Error', data: [] });
-						})
+					$http({
+						method: method,
+						url: link,
+						data: data
+					}).then(res => {
+						resolve({ msg: 'OK', data: res.data });
+					}).catch(err => {
+						reject({ msg: 'Error', data: [], err: err });
+					})
 				});
 			}
 
+			svc.addStudent = function (student) {
+				return sendData(student, 'POST', api);
+			}
+
 			svc.updateStudent = function (student) {
-				return $q(function (resolve, reject) {
-					$http.put(api)
-						.then(res => {
-							resolve({ msg: 'OK', data: res.data });
-						}).catch(err => {
-							reject({ msg: 'Error', data: [] });
-						})
-				});
+				return sendData(student, 'PUT', api + student.id);
 			}
 
 			return svc;
@@ -49,49 +49,68 @@
 			$scope.formMethod = 'POST';
 			$scope.modalTitle = 'Add New Student data:';
 			$scope.modalBtnTxt = 'Add';
-			$scope.curStudent = {
-				id: '',
-				name: '',
-				rollNo: '',
-				email: '',
-				date: '',
-				isMale: true
-			};
+			$scope.disableButton = false;
+			$scope.curStudent = null;
 
-			studentsvc.getStudents()
-				.then(res => {
-					$scope.students = res.data;
-					console.log(res);
-				}).catch(err => {
-					console.log('Error occured:', err.msg);
-					$scope.students = [];
-				});
+			const modal = $('#student-form-modal');
+
+			function refreshStudentList() {
+				studentsvc.getStudents()
+					.then(res => {
+						$scope.students = res.data;
+						console.log(res);
+					}).catch(err => {
+						console.log('Error occured:', err.msg);
+						$scope.students = [];
+					});
+			}
+			refreshStudentList();
 
 			$scope.updateStudent = function (student) {
 				$scope.formMethod = 'PUT';
-				console.log(student, $scope.formMethod);
 				$scope.modalTitle = 'Update Student data:';
 				$scope.modalBtnTxt = 'Update';
 				$scope.curStudent = student;
+				$scope.curStudent.ngdate = new Date(student.date);
 			}
 
 			$scope.addStudent = function (student) {
 				$scope.formMethod = 'POST';
-				console.log($scope.formMethod);
 				$scope.modalTitle = 'Add Student data:';
 				$scope.modalBtnTxt = 'Add';
-				$scope.curStudent = {
-					id: '',
-					name: '',
-					rollNo: '',
-					email: '',
-					date: '',
-					isMale: true
-				};;
+				$scope.curStudent = null;
 			}
 
 			$scope.submit = function () {
 				console.log($scope.curStudent);
+				$scope.disableButton = true;
+				console.log($scope.curStudent.ngdate);
+				$scope.curStudent.date = ''
+					+ $scope.curStudent.ngdate.getFullYear()
+					+ '-' + ($scope.curStudent.ngdate.getMonth() + 1)
+					+ '-' + $scope.curStudent.ngdate.getDate();
+
+				let svcFn = $scope.formMethod == 'POST'
+					? studentsvc.addStudent : studentsvc.updateStudent;
+				svcFn({
+					id: $scope.curStudent.id,
+					rollNo: $scope.curStudent.rollNo,
+					name: $scope.curStudent.name,
+					email: $scope.curStudent.email,
+					date: $scope.curStudent.date,
+					age: $scope.curStudent.age,
+					isMale: $scope.curStudent.isMale
+				}).then(res => {
+					console.log(res);
+					modal.modal('hide');
+					$scope.disableButton = false;
+					refreshStudentList();
+				}).catch(err => {
+					modal.modal('hide');
+					$scope.disableButton = false;
+					console.log('Error:', err);
+				});
+
 			}
 		}
 	]);
